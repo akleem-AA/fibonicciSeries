3ca5041c5851378610fe9a94f082430a3c381042
akleem <74227145+akleem-AA@users.noreply.github.com>
2023-07-27 16:26:50 +0530

    Create FibonacciSeries.js
    create a normal function to find fibonacci number

---
+++ FibonacciSeries.js
@@ -0,0 +1,9 @@
+function fibonicc(n){
+    let fibo = [0,1];
+    for(let i = 2;i<n;i++){
+        fibo[i] = fibo[i -1] +fibo[i-2]
+    }
+    return fibo;
+    
+}
+console.log(fibonicc(7)) //output [0,1,1,2,3,5,8]
